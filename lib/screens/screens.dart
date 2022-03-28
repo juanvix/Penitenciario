e4f734f8329532d0f60d9798e412549bc9e105a4
unknown <juanvix@gmail.com>
2022-03-28 21:45:24 +0200

    Update screens.dart

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -3,3 +3,4 @@ export 'package:penitenciario/screens/login.dart';
 export 'package:penitenciario/screens/home.dart';
 export 'package:penitenciario/screens/internos.dart';
 export 'package:penitenciario/screens/loading.dart';
+export 'package:penitenciario/screens/register.dart';
